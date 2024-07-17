1298aee29f0b93b8d52fd11b62df668c95886484
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-07-18 03:35:47 +0530

    working of serviceworker

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public', // Ensure Vite knows where to find public assets
-  build: {
-    outDir: 'dist' // Ensure build output directory matches Vercel config
-  },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-redux', '@reduxjs/toolkit'],
-  }
+  },
+
 })
+
